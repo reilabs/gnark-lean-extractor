fbededb87934958c893a2fc3b6a26dab1013e9df
Giuseppe <8973725+Eagle941@users.noreply.github.com>
2023-06-20 16:08:36 +0100

    Fixed format golang

--- extractor/extractor_test.go
+++ extractor/extractor_test.go
@@ -34,7 +34,7 @@ func TestExtractor(t *testing.T) {
 	}
 	defineExample(&api)
 	circuit := ExCircuit{
-		Inputs:  []ExArgs{{"i1",1}, {"i2",1}},
+		Inputs:  []ExArgs{{"i1", 1}, {"i2", 1}},
 		Gadgets: api.Gadgets,
 		Code:    api.Code,
 	}
