ccc76a70bd88e9092b495322b657d8af460977a6
Giuseppe <8973725+Eagle941@users.noreply.github.com>
2023-07-01 16:19:12 +0100

    Semaphore wip

--- extractor/extractor_test.go
+++ extractor/extractor_test.go
@@ -19,6 +19,7 @@ type Semaphore struct {
 	SignalHash        frontend.Variable `gnark:",public"`
 	ExternalNullifier frontend.Variable `gnark:",public"`
 
+	// Outputs to check
 	NullifierHash     frontend.Variable `gnark:",public"`
 	MTRoot            frontend.Variable `gnark:",public"`
 }
@@ -84,104 +85,104 @@ func TestSemaphore(t *testing.T) {
 	}
 }
 
-type CircuitWithParameter struct {
-	In    frontend.Variable `gnark:",public"`
-	Param int
-}
+// type CircuitWithParameter struct {
+// 	In    frontend.Variable `gnark:",public"`
+// 	Param int
+// }
 
-func (circuit *CircuitWithParameter) AbsDefine(api abstractor.API) error {
-	api.AssertIsEqual(circuit.Param, circuit.In)
+// func (circuit *CircuitWithParameter) AbsDefine(api abstractor.API) error {
+// 	api.AssertIsEqual(circuit.Param, circuit.In)
 
-	return nil
-}
+// 	return nil
+// }
 
-func (circuit CircuitWithParameter) Define(api frontend.API) error {
-	return abstractor.Concretize(api, &circuit)
-}
+// func (circuit CircuitWithParameter) Define(api frontend.API) error {
+// 	return abstractor.Concretize(api, &circuit)
+// }
 
-func TestCircuitWithParameter(t *testing.T) {
-	paramValue := 20
-	assignment := CircuitWithParameter{}
-	assignment.Param = paramValue
-	assert.Equal(t, assignment.Param, paramValue, "assignment.Param is a const and should be 20.")
-	err := CircuitToLean(&assignment, ecc.BW6_756)
-	if err != nil {
-		fmt.Println("CircuitToLean error!")
-		fmt.Println(err.Error())
-	}
-}
+// func TestCircuitWithParameter(t *testing.T) {
+// 	paramValue := 20
+// 	assignment := CircuitWithParameter{}
+// 	assignment.Param = paramValue
+// 	assert.Equal(t, assignment.Param, paramValue, "assignment.Param is a const and should be 20.")
+// 	err := CircuitToLean(&assignment, ecc.BW6_756)
+// 	if err != nil {
+// 		fmt.Println("CircuitToLean error!")
+// 		fmt.Println(err.Error())
+// 	}
+// }
 
-type MerkleRecover struct {
-	Root    frontend.Variable     `gnark:",public"`
-	Element frontend.Variable     `gnark:",public"`
-	Path    [20]frontend.Variable `gnark:",secret"`
-	Proof   [20]frontend.Variable `gnark:",secret"`
-}
+// type MerkleRecover struct {
+// 	Root    frontend.Variable     `gnark:",public"`
+// 	Element frontend.Variable     `gnark:",public"`
+// 	Path    [20]frontend.Variable `gnark:",secret"`
+// 	Proof   [20]frontend.Variable `gnark:",secret"`
+// }
 
-func (circuit *MerkleRecover) AbsDefine(api abstractor.API) error {
-	hash := api.DefineGadget("hash", 2, func(api abstractor.API, args ...frontend.Variable) []frontend.Variable {
-		return []frontend.Variable{api.Mul(args[0], args[1])}
-	})
+// func (circuit *MerkleRecover) AbsDefine(api abstractor.API) error {
+// 	hash := api.DefineGadget("hash", 2, func(api abstractor.API, args ...frontend.Variable) []frontend.Variable {
+// 		return []frontend.Variable{api.Mul(args[0], args[1])}
+// 	})
 
-	current := circuit.Element
-	for i := 0; i < len(circuit.Path); i++ {
-		leftHash := hash.Call(current, circuit.Proof[i])[0]
-		rightHash := hash.Call(circuit.Proof[i], current)[0]
-		current = api.Select(circuit.Path[i], rightHash, leftHash)
-	}
-	api.AssertIsEqual(current, circuit.Root)
+// 	current := circuit.Element
+// 	for i := 0; i < len(circuit.Path); i++ {
+// 		leftHash := hash.Call(current, circuit.Proof[i])[0]
+// 		rightHash := hash.Call(circuit.Proof[i], current)[0]
+// 		current = api.Select(circuit.Path[i], rightHash, leftHash)
+// 	}
+// 	api.AssertIsEqual(current, circuit.Root)
 
-	return nil
-}
+// 	return nil
+// }
 
-func (circuit MerkleRecover) Define(api frontend.API) error {
-	return abstractor.Concretize(api, &circuit)
-}
+// func (circuit MerkleRecover) Define(api frontend.API) error {
+// 	return abstractor.Concretize(api, &circuit)
+// }
 
-func TestMerkleRecover(t *testing.T) {
-	assignment := MerkleRecover{}
-	err := CircuitToLean(&assignment, ecc.BW6_756)
-	if err != nil {
-		fmt.Println("CircuitToLean error!")
-		fmt.Println(err.Error())
-	}
-}
+// func TestMerkleRecover(t *testing.T) {
+// 	assignment := MerkleRecover{}
+// 	err := CircuitToLean(&assignment, ecc.BW6_756)
+// 	if err != nil {
+// 		fmt.Println("CircuitToLean error!")
+// 		fmt.Println(err.Error())
+// 	}
+// }
 
-type TwoGadgets struct {
-	In_1 frontend.Variable
-	In_2 frontend.Variable
-}
+// type TwoGadgets struct {
+// 	In_1 frontend.Variable
+// 	In_2 frontend.Variable
+// }
 
-func (circuit *TwoGadgets) AbsDefine(api abstractor.API) error {
-	my_widget := api.DefineGadget("my_widget", 2, func(api abstractor.API, args ...frontend.Variable) []frontend.Variable {
-		sum := api.Add(args[0], args[1])
-		mul := api.Mul(args[0], args[1])
-		r := api.Div(sum, mul)
-		return []frontend.Variable{r}
-	})
-	my_snd_widget := api.DefineGadget("my_snd_widget", 2, func(api abstractor.API, args ...frontend.Variable) []frontend.Variable {
-		mul := api.Mul(args[0], args[1])
-		snd := my_widget.Call(args[0], args[1])
-		r := api.Mul(mul, snd[0])
-		return []frontend.Variable{r}
-	})
+// func (circuit *TwoGadgets) AbsDefine(api abstractor.API) error {
+// 	my_widget := api.DefineGadget("my_widget", 2, func(api abstractor.API, args ...frontend.Variable) []frontend.Variable {
+// 		sum := api.Add(args[0], args[1])
+// 		mul := api.Mul(args[0], args[1])
+// 		r := api.Div(sum, mul)
+// 		return []frontend.Variable{r}
+// 	})
+// 	my_snd_widget := api.DefineGadget("my_snd_widget", 2, func(api abstractor.API, args ...frontend.Variable) []frontend.Variable {
+// 		mul := api.Mul(args[0], args[1])
+// 		snd := my_widget.Call(args[0], args[1])
+// 		r := api.Mul(mul, snd[0])
+// 		return []frontend.Variable{r}
+// 	})
 
-	sum := api.Add(circuit.In_1, circuit.In_2)
-	prod := api.Mul(circuit.In_1, circuit.In_2)
-	my_snd_widget.Call(sum, prod)
+// 	sum := api.Add(circuit.In_1, circuit.In_2)
+// 	prod := api.Mul(circuit.In_1, circuit.In_2)
+// 	my_snd_widget.Call(sum, prod)
 
-	return nil
-}
+// 	return nil
+// }
 
-func (circuit TwoGadgets) Define(api frontend.API) error {
-	return abstractor.Concretize(api, &circuit)
-}
+// func (circuit TwoGadgets) Define(api frontend.API) error {
+// 	return abstractor.Concretize(api, &circuit)
+// }
 
-func TestTwoGadgets(t *testing.T) {
-	assignment := TwoGadgets{}
-	err := CircuitToLean(&assignment, ecc.BW6_756)
-	if err != nil {
-		fmt.Println("CircuitToLean error!")
-		fmt.Println(err.Error())
-	}
-}
+// func TestTwoGadgets(t *testing.T) {
+// 	assignment := TwoGadgets{}
+// 	err := CircuitToLean(&assignment, ecc.BW6_756)
+// 	if err != nil {
+// 		fmt.Println("CircuitToLean error!")
+// 		fmt.Println(err.Error())
+// 	}
+// }
