3f161e2d5e2fd0aacbf491ae55d15beaba058016
Giuseppe <8973725+Eagle941@users.noreply.github.com>
2023-09-25 19:38:57 +0100

    Fix

--- extractor/interface.go
+++ extractor/interface.go
@@ -15,39 +15,35 @@ import (
 	"golang.org/x/exp/slices"
 )
 
-func RawCall(api frontend.API, gadget abstractor.GadgetDefinition) interface{} {
+// Call is used to call a Gadget which returns frontend.Variable (i.e. a single element `F` in Lean)
+func Call(api frontend.API, gadget abstractor.GadgetDefinition) frontend.Variable {
 	if abs, ok := api.(*CodeExtractor); ok {
-		return abs.Call(gadget)
+		return abs.Call(gadget).(frontend.Variable)
 	} else {
-		return gadget.DefineGadget(api)
+		return gadget.DefineGadget(api).(frontend.Variable)
 	}
 }
 
 // CallVoid is used to call a Gadget which doesn't return anything
 func CallVoid(api frontend.API, gadget abstractor.GadgetDefinition) {
-	RawCall(api, gadget)
-}
-
-// Call is used to call a Gadget which returns frontend.Variable (i.e. a single element `F` in Lean)
-func Call(api frontend.API, gadget abstractor.GadgetDefinition) frontend.Variable {
-	return RawCall(api, gadget).(frontend.Variable)
+	Call(api, gadget)
 }
 
 // Call1 is used to call a Gadget which returns []frontend.Variable (i.e. `Vector F d` in Lean)
 func Call1(api frontend.API, gadget abstractor.GadgetDefinition) []frontend.Variable {
-	return RawCall(api, gadget).([]frontend.Variable)
+	return Call(api, gadget).([]frontend.Variable)
 }
 
 // Call2 is used to call a Gadget which returns a [][]frontend.Variable
 // (i.e. `Vector (Vector F a) b` in Lean)
 func Call2(api frontend.API, gadget abstractor.GadgetDefinition) [][]frontend.Variable {
-	return RawCall(api, gadget).([][]frontend.Variable)
+	return Call(api, gadget).([][]frontend.Variable)
 }
 
 // Call3 is used to call a Gadget which returns a [][][]frontend.Variable
 // (i.e. `Vector (Vector (Vector F a) b) c` in Lean)
 func Call3(api frontend.API, gadget abstractor.GadgetDefinition) [][][]frontend.Variable {
-	return RawCall(api, gadget).([][][]frontend.Variable)
+	return Call(api, gadget).([][][]frontend.Variable)
 }
 
 // CircuitToLeanWithName exports a `circuit` to Lean over a `field` with `namespace`
