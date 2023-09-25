c136f07c4af02eaec4688231c8f333024f57eace
Giuseppe <8973725+Eagle941@users.noreply.github.com>
2023-09-25 19:25:27 +0100

    refactor call

--- extractor/interface.go
+++ extractor/interface.go
@@ -15,17 +15,7 @@ import (
 	"golang.org/x/exp/slices"
 )
 
-// CallVoid is used to call a Gadget which doesn't return anything
-func CallVoid(api frontend.API, gadget abstractor.GadgetDefinition) {
-	if abs, ok := api.(*CodeExtractor); ok {
-		abs.Call(gadget)
-	} else {
-		gadget.DefineGadget(api)
-	}
-}
-
-// Call is used to call a Gadget which returns frontend.Variable (i.e. a single element `F` in Lean)
-func Call(api frontend.API, gadget abstractor.GadgetDefinition) frontend.Variable {
+func privateCall(api frontend.API, gadget abstractor.GadgetDefinition) interface{} {
 	if abs, ok := api.(*CodeExtractor); ok {
 		return abs.Call(gadget).(frontend.Variable)
 	} else {
@@ -33,33 +23,31 @@ func Call(api frontend.API, gadget abstractor.GadgetDefinition) frontend.Variabl
 	}
 }
 
+// CallVoid is used to call a Gadget which doesn't return anything
+func CallVoid(api frontend.API, gadget abstractor.GadgetDefinition) {
+	privateCall(api, gadget)
+}
+
+// Call is used to call a Gadget which returns frontend.Variable (i.e. a single element `F` in Lean)
+func Call(api frontend.API, gadget abstractor.GadgetDefinition) frontend.Variable {
+	return privateCall(api, gadget).(frontend.Variable)
+}
+
 // Call1 is used to call a Gadget which returns []frontend.Variable (i.e. `Vector F d` in Lean)
 func Call1(api frontend.API, gadget abstractor.GadgetDefinition) []frontend.Variable {
-	if abs, ok := api.(*CodeExtractor); ok {
-		return abs.Call(gadget).([]frontend.Variable)
-	} else {
-		return gadget.DefineGadget(api).([]frontend.Variable)
-	}
+	return privateCall(api, gadget).([]frontend.Variable)
 }
 
 // Call2 is used to call a Gadget which returns a [][]frontend.Variable
 // (i.e. `Vector (Vector F a) b` in Lean)
 func Call2(api frontend.API, gadget abstractor.GadgetDefinition) [][]frontend.Variable {
-	if abs, ok := api.(*CodeExtractor); ok {
-		return abs.Call(gadget).([][]frontend.Variable)
-	} else {
-		return gadget.DefineGadget(api).([][]frontend.Variable)
-	}
+	return privateCall(api, gadget).([][]frontend.Variable)
 }
 
 // Call3 is used to call a Gadget which returns a [][][]frontend.Variable
 // (i.e. `Vector (Vector (Vector F a) b) c` in Lean)
 func Call3(api frontend.API, gadget abstractor.GadgetDefinition) [][][]frontend.Variable {
-	if abs, ok := api.(*CodeExtractor); ok {
-		return abs.Call(gadget).([][][]frontend.Variable)
-	} else {
-		return gadget.DefineGadget(api).([][][]frontend.Variable)
-	}
+	return privateCall(api, gadget).([][][]frontend.Variable)
 }
 
 // CircuitToLeanWithName exports a `circuit` to Lean over a `field` with `namespace`
